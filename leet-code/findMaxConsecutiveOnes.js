9fd2888dd68fa2467915e09c2ac3f0ff9cadfbd4
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-01-24 23:09:59 -0800

    Update findMaxConsecutiveOnes.js

--- leet-code/findMaxConsecutiveOnes.js
+++ leet-code/findMaxConsecutiveOnes.js
@@ -1,1 +1,10 @@
-
+var findMaxConsecutiveOnes = function(nums) {
+    let maxCount = 0;
+    let count = 0;
+    nums.forEach(function(num){
+        if(num===1) count++;
+        else count =0;
+        if(count>maxCount) maxCount = count;
+    });
+    return maxCount;
+};
