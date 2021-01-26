61c02cc7e878e38328b210fb8eac4b3d7cacc3c8
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-01-25 22:00:32 -0800

    Update findDisappearedNumbers.js

--- leet-code/findDisappearedNumbers.js
+++ leet-code/findDisappearedNumbers.js
@@ -1,1 +1,22 @@
-
+var findDisappearedNumbers = function(nums) {
+    const maxLength = nums.length;
+    
+    function getResultArray(max){
+        let newArray = [];
+        for (let i=1; i<=max; i++){
+            newArray.push(i);
+        }
+        return newArray;
+    }
+    
+    const result = getResultArray(maxLength);
+    
+    for(let i=0; i<maxLength;i++){
+        const ele = nums[i];
+        const indexOfResult = result.indexOf(ele);
+        if(indexOfResult >-1){
+            result.splice(indexOfResult, 1);
+        }
+    }
+    return result;
+};
