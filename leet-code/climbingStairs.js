3fa238518688006516e339a80be3aad1925f2dcc
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-01-26 22:32:07 -0800

    Update climbingStairs.js

--- leet-code/climbingStairs.js
+++ leet-code/climbingStairs.js
@@ -1,1 +1,13 @@
-
+let climbStairs = function(n, memo = {}) {    
+    if (n<4){
+         return n;
+    } else{
+        if (n in memo){
+            return memo[n];
+        } else{
+            let results = climbStairs(n-1, memo)+ climbStairs(n -2, memo)
+            memo[n] = results;
+            return results;
+        }
+    }
+};
