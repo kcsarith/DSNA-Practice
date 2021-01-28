4bcd056be82f5813314c0dff9a7b449ffaf5a330
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-01-27 22:24:27 -0800

    Update findMin.js

--- leet-code/findMin.js
+++ leet-code/findMin.js
@@ -1,1 +1,10 @@
-
+var findMin = function(nums) {
+    let min = nums[0];
+    for(let i =1; i<nums.length; i++){
+        const currentNum = nums[i];
+        if(currentNum<min){
+            min = currentNum
+        }
+    }
+    return min;
+};
