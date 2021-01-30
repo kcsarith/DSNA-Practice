af2ddf9761ec16ed402d659dee29717c977257b8
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-01-29 21:03:07 -0800

    Update sortedArrayToBST.js

--- leet-code/sortedArrayToBST.js
+++ leet-code/sortedArrayToBST.js
@@ -1,1 +1,14 @@
-
+var sortedArrayToBST = function(nums) {
+    if(nums.length===0) return null;
+    
+    let rootIndex = Math.floor(nums.length/2);
+    let rootNode = new TreeNode(nums[rootIndex]);
+    
+    let leftSorted = nums.slice(0, rootIndex);
+    let rightSorted = nums.slice(rootIndex+1);
+    
+    rootNode.left = sortedArrayToBST(leftSorted);
+    rootNode.right = sortedArrayToBST(rightSorted);
+    
+    return rootNode;
+};
