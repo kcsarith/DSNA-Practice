a473a8cdc6a9e3e6e7895217769b3baa77a19c73
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-01-30 23:00:46 -0800

    Update buildTreeTraversals.js

--- leet-code/buildTreeTraversals.js
+++ leet-code/buildTreeTraversals.js
@@ -1,1 +1,16 @@
-
+var buildTree = function(preorder, inorder) {
+    if(preorder.length ===0 && inorder.length ===0) return null;
+    let rootNode = new TreeNode(preorder[0]);
+    let rootIndex = inorder.indexOf(rootNode.val);
+    
+    let leftInorder = inorder.slice(0, rootIndex); //[9]
+    let rightInorder = inorder.slice(rootIndex+1);//[15,20,7]
+    
+    let leftPreorder = preorder.filter(ele=>leftInorder.includes(ele));
+    let rightPreorder = preorder.filter(ele=>rightInorder.includes(ele));
+    
+    rootNode.left = buildTree(leftPreorder, leftInorder);
+    rootNode.right = buildTree(rightPreorder, rightInorder);
+    
+    return rootNode;
+};
