bb9a23afed334d8bf8172b3f7c598745965a90fe
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-02-05 23:33:37 -0800

    Update twoNumbersSum.js

--- algo-expert/twoNumbersSum.js
+++ algo-expert/twoNumbersSum.js
@@ -1,5 +1,15 @@
 function twoNumberSum(array, targetSum) {
   // Write your code here.
+  	const memo = {};
+	for (let i =0; i<array.length;i++){
+		const num = array[i];
+		const difference = targetSum-num;
+		if(memo[difference]){
+			return [num, difference];
+		}
+		memo[num]=true;
+	}
+	return [];
 }
 
 // Do not edit the line below.
