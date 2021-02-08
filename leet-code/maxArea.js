2322e6291e2987ea604b1cc6f20e53e037998621
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-02-07 22:19:35 -0800

    Update maxArea.js

--- leet-code/maxArea.js
+++ leet-code/maxArea.js
@@ -1,3 +1,15 @@
+//Brute Force
 var maxArea = function(height) {
-    
+    let mostWater =0
+    for(let i=0; i<height.length;i++){
+        const currentHeight = height[i];
+        for(let j=i+1; j<height.length;j++){
+            const nextHeight = height[j];
+            let smallerHeight=currentHeight;
+            if(currentHeight>nextHeight) smallerHeight=nextHeight;
+            const currentWater = smallerHeight*(j-i);
+            if(currentWater>mostWater) mostWater = currentWater
+        }
+    }
+    return mostWater;
 };
