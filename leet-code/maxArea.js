ffbb9eb6e7203636ca411b8f9e135ec73efd3728
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-02-07 23:06:08 -0800

    Update maxArea.js
    Brute Force vs Optimal Solutoin

--- leet-code/maxArea.js
+++ leet-code/maxArea.js
@@ -1,5 +1,4 @@
-//Brute Force
-var maxArea = function(height) {
+var maxAreaBrute = function(height) {
     let mostWater =0
     for(let i=0; i<height.length;i++){
         const currentHeight = height[i];
@@ -9,6 +8,26 @@ var maxArea = function(height) {
             if(currentHeight>nextHeight) smallerHeight=nextHeight;
             const currentWater = smallerHeight*(j-i);
             if(currentWater>mostWater) mostWater = currentWater
+        }
+    }
+    return mostWater;
+};
+
+
+var maxArea = function(height) {
+    let mostWater =0;
+    let leftIndex = 0;
+    let rightIndex = height.length-1;
+    
+    while(leftIndex<rightIndex){
+        let smallerHeight=height[leftIndex];
+        if(height[leftIndex]>height[rightIndex]) smallerHeight=height[rightIndex];
+        const currentWater = smallerHeight*(rightIndex-leftIndex);
+        if(currentWater>mostWater) mostWater = currentWater;
+        if(height[leftIndex]<height[rightIndex]){
+            leftIndex++
+        } else{
+            rightIndex--
         }
     }
     return mostWater;
