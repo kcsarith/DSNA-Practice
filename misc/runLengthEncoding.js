311beebf80ac33f63c0387d71434b8af6331a1c5
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-02-11 23:36:39 -0800

    Update runLengthEncoding.js

--- misc/runLengthEncoding.js
+++ misc/runLengthEncoding.js
@@ -1,1 +1,20 @@
+function runLengthEncoding(str) {
+    let encodedString = '';
+    let letterCount = 1;
+    for (let i = 0; i < str.length; i++) {
+        const currentLetter = str[i];
+        const nextLetter = str[i + 1];
+        if (currentLetter === nextLetter && letterCount < 9) letterCount++;
+        else if (currentLetter === nextLetter && letterCount > 9) {
+            encodedString += `9${currentLetter}`;
+            letterCount = 1;
+        }
+        else {
+            encodedString += `${letterCount}${currentLetter}`;
+            letterCount = 1;
+        }
+    }
+    return encodedString;
+}
 
+console.log(runLengthEncoding('AAAAAAAAAAAAABBCCCCDD'))
