f5d3c0b7dd577a5354814fa067c73a9f31442d17
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-03-02 22:14:19 -0800

    Update drone_space.py

--- misc/drone_space.py
+++ misc/drone_space.py
@@ -1,1 +1,19 @@
+// starter code below
+import random
 
+random.seed(1) # Setting random number generator seed for repeatability
+
+NUM_DRONES = 10000
+AIRSPACE_SIZE = 128000 # Meters.
+CONFLICT_RADIUS = 500 # Meters.
+
+def count_conflicts(drones, conflict_radius):
+  raise Exception("Not Implemented")
+
+def gen_coord():
+  return int(random.random() * AIRSPACE_SIZE)
+
+positions = [[gen_coord(), gen_coord()] for i in range(NUM_DRONES)]
+conflicts = count_conflicts(positions, CONFLICT_RADIUS)
+
+print("Drones in conflict: {}".format(conflicts))
