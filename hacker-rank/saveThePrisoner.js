4a28db7b9fa50ea6168af9fa15ee3be7784d4acf
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-04-03 23:37:30 -0700

    Update saveThePrisoner.js

--- hacker-rank/saveThePrisoner.js
+++ hacker-rank/saveThePrisoner.js
@@ -1,1 +1,7 @@
-
+function saveThePrisoner(n, m, s) {
+    const endValue = m+s-1;
+    const remainder = endValue%n;
+    if(n>=endValue) return endValue;
+    if(remainder ===0) return n;
+    return remainder;
+}
