acd68c64f9acec0647a7cd698bb2298654940a94
Krisna Sarith <45773785+kcsarith@users.noreply.github.com>
2021-05-03 22:21:04 -0700

    Update 20122020.js

--- hacker-rank/20122020.js
+++ hacker-rank/20122020.js
@@ -1,1 +1,29 @@
+function no20122020(range) {
+    const minNumber = range[0];
+    const maxNumber = range[1];
+    let currentExponentThree = 1;
+    const exponentFives = { 0: 1 };
+    let exponentFiveIncrement = 0;
+    const results = [];
+    while (currentExponentThree <= maxNumber * 3) {
+        exponentFiveIncrement = 0;
+        while (true) {
+            if (!exponentFives[exponentFiveIncrement]) {
+                if (exponentFiveIncrement !== 0) exponentFives[exponentFiveIncrement] = Math.pow(5, exponentFiveIncrement);
+            }
+            const exponentProducts = currentExponentThree * exponentFives[exponentFiveIncrement];
+            const greaterThanMin = exponentProducts >= minNumber;
+            const lessThanMax = exponentProducts <= maxNumber;
+            if (exponentProducts > maxNumber) break;
+            if (greaterThanMin && lessThanMax) {
+                results.push(exponentProducts);
+            }
+            exponentFiveIncrement++;
+        }
+        currentExponentThree *= 3;
+    }
+    console.log(exponentFives);
+    return results;
+}
 
+console.log(no20122020([0, 2000000]));
